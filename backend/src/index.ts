2777e4445143c1a59c65adfd38ba43046c24fee6
sebastian <sebastian.dev0708gmail.com>
2025-08-31 14:37:07 -0500

    Mejorando logs

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,8 +3,9 @@ import server from "./server";
 import { envs } from './config/envs';
 
 server.listen(envs.PORT, () => {
-  console.log(colors.bgBlack.italic(
-    `[server]: Server is running at http://localhost:${envs.PORT}`
-  )
+  console.log(
+    colors.green.bold("[index]🎓 Graduation project ") +
+    colors.cyan("running on port ") +
+    colors.yellow(envs.PORT.toString())
   );
 });
