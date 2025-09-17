66087fbfb3f47c53de66019bfbd34b50f4ce4f39
Xhall1 <gridoracones@gmail.com>
2025-09-17 14:43:17 -0500

    feat: Responsive dashboard layout

--- frontend/app/(dashboard)/layout.tsx
+++ frontend/app/(dashboard)/layout.tsx
@@ -4,8 +4,9 @@ export default function DashboardLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <div> dashboard {children} </div>
-    </html>
+      <div className="h-screen flex">
+        <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] bg-red-400">l</div> {/* LEFT SIDE */}
+        <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-blue-400">r</div> {/* RIGHT SIDE */}
+      </div>
   );
 }
