066ec7653eb38bf4ef67a580428695074ea83e79
Xhall1 <gridoracones@gmail.com>
2025-09-18 17:47:54 -0500

    feat: Adding Radial Chart with Recharts

--- frontend/app/(dashboard)/admin/page.tsx
+++ frontend/app/(dashboard)/admin/page.tsx
@@ -1,10 +1,11 @@
+import CountCharts from "@/app/components/CountCharts";
 import UserCard from "@/app/components/UserCard";
 
 export default function AdminPage() {
   return (
-    <div className="p-4 flex gap-4 flex-col md:flex-row">
+    <div className="p-4 flex gap-4 flex-col md:flex-col">
       {/* LEFT */}
-      <div className="w-full lg:w-2/3">
+      <div className="w-full lg:w-2/3 flex flex-col gap-8">
         {/* USER CARDS */}
         <div className="flex gap-4 justify-between flex-wrap">
           <UserCard type="Total proyectos grado registrados"/>
@@ -13,6 +14,19 @@ export default function AdminPage() {
           <UserCard type="Profesores/directores activos"/>
         </div>
       </div>
+      {/* MIDDLE CHARTS */}
+      <div className="flex gap-4 flex-col lg:flex-row">
+        {/* COUNT CHART */}
+        <div className="w-full lg:w-1/3 h-[450px]">
+          <CountCharts />
+        </div>
+        {/* ATTENDANCE CHART */}
+        <div className="w-full lg:w-2/3 h-[450px]">
+        
+        </div>
+      </div>
+      {/* BOTTOM CHARTS */}
+      <div className=""></div>
       {/* RIGHT */}
       <div className="w-full lg:w-1/3">r</div>
     </div>
