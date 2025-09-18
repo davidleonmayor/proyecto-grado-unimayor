b3548d342d2426066e32c731d159f46b37377a74
Xhall1 <gridoracones@gmail.com>
2025-09-18 15:59:00 -0500

    feat: Adding Admin dashboard design

--- frontend/app/(dashboard)/admin/page.tsx
+++ frontend/app/(dashboard)/admin/page.tsx
@@ -1,6 +1,20 @@
+import UserCard from "@/app/components/UserCard";
 
 export default function AdminPage() {
   return (
-    <div>AdminPage</div>
+    <div className="p-4 flex gap-4 flex-col md:flex-row">
+      {/* LEFT */}
+      <div className="w-full lg:w-2/3">
+        {/* USER CARDS */}
+        <div className="flex gap-4 justify-between flex-wrap">
+          <UserCard type="Total proyectos grado registrados"/>
+          <UserCard type="Proyectos en curso"/>
+          <UserCard type="Proyectos finalizados"/>
+          <UserCard type="Profesores/directores activos"/>
+        </div>
+      </div>
+      {/* RIGHT */}
+      <div className="w-full lg:w-1/3">r</div>
+    </div>
   )
 }
