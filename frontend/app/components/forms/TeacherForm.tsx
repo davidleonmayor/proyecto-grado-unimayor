78f52403778d09537b021bc7127729fc3e9c2654
Xhall1 <gridoracones@gmail.com>
2025-09-21 01:08:51 -0500

    feat: Adding Form Design

---
+++ frontend/app/components/forms/TeacherForm.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+const TeacherForm = ({type, data}:{type:"create" | "update"; data?:any }) => {
+    return (
+        <form className="">
+            <input type="text" defaultValue={} />
+        </form>
+    );
+};
+
+export default TeacherForm;
