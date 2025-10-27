41f2a381a59636a5190a9d291f2f7bd3813c6638
davidleonmayor <jd.leon@unimayor.edu.co>
2025-10-27 06:33:59 -0500

    fix(auth): auth router, but the schama doesn't have a property to store the password

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,34 +1,33 @@
 import { Application } from "express";
-import { AuthRoutes } from "../auth";
-import { ExampleRoutes } from "../example";
-
+import { AuthRoutes } from "../auth/auth.routes";
+import { ExampleRoutes } from "../example/example.routes";
 
 /**
  * Este archivo centraliza todas las rutas de la aplicación.
- * 
+ *
  * Cada módulo (usuarios, pagos, deudas, etc.) tiene su propia clase de rutas
  * y se importa aquí para montarlas en la instancia principal de Express.
- * 
+ *
  * Esto permite:
  * 1. Mantener el servidor limpio y modular.
  * 2. Escalar fácilmente agregando nuevos módulos de rutas.
  * 3. Aplicar middlewares globales o específicos de cada módulo de forma organizada.
- * 
+ *
  * Uso:
  * En el servidor principal:
  *    Routes.init(app);
  */
 
 export class Routes {
-
     public static init(app: Application) {
-
         //Aquí agregan las rutas de cada modulo.
         const authRoutes = new AuthRoutes();
         const exampleRoutes = new ExampleRoutes();
 
-        app.use('/api/auth', authRoutes.router);
-        app.use('/api/example', exampleRoutes.router)
+        authRoutes.initRoutes();
+        exampleRoutes.initRoutes();
 
+        app.use("/api/auth", authRoutes.router);
+        app.use("/api/example", exampleRoutes.router);
     }
 }
